3955f22b3e737583b861705cd35dd83db028909f
Mihail Marian <mihailmarian12@gmail.com>
2020-11-17 19:37:18 +0200

    added hover styling to nav links

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,6 +3,11 @@ import html from "./navbar.html";
 
 const navbarLoad = () => {
   document.body.insertAdjacentHTML("beforeend", html);
+
+  /*
+  document.querySelector("li").onclick((event) => {
+    console.log(event.target);
+  });*/
 };
 
 export { navbarLoad };
