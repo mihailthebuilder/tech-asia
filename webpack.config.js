d0d691a7eef70d115298cc791c0be76e87d7c42c
Mihail Marian <mihailmarian12@gmail.com>
2020-11-17 12:22:09 +0200

    Installed file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+  entry: './src/index.js',
+  
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        use: 'file-loader'
+      },
+        
+    ]
   }
 }
