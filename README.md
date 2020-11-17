05cdd24d87d4e1b5629c5e843dede745a8d9b1e5
Mihail Marian <mihailmarian12@gmail.com>
2020-11-17 13:22:07 +0200

    Ran prettier across all files

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# Tech News Asia #
+# Tech News Asia
 
 A news site about tech stuff in Asia. A project that enables me to recap what I learned from The Odin Project about Webpack and a few other minor things I learned along the way such as...
-* Prettier
-* Sass
-* [Coolors](https://coolors.co/ "Coolors")
-* Pure JavaScript, no jQuery
-* [Figma](https://www.figma.com/ "Figma")
-* Git using Bash
+
+- Prettier
+- Sass
+- [Coolors](https://coolors.co/ "Coolors")
+- Pure JavaScript, no jQuery
+- [Figma](https://www.figma.com/ "Figma")
+- Git using Bash
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,37 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  
+  entry: "./src/index.js",
+
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
 
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
       },
 
       {
         test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: 'file-loader'
+        use: "file-loader",
       },
-        
-    ]
-  }
-}
+    ],
+  },
+};
