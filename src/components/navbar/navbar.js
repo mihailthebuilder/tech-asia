a7853f8423191e69b6adf1bc78a9ffe25b7ffd53
Mihail Marian <mihailmarian12@gmail.com>
2020-11-18 13:36:48 +0200

    Click styling

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,28 @@
 import "./navbar.scss";
 import html from "./navbar.html";
 
+import articleLoad from "../sidebar/sidebar.js";
+
 const navbarLoad = () => {
   document.body.insertAdjacentHTML("beforeend", html);
 
-  document.querySelectorAll("nav li").forEach((element) => {
-    element.onclick = (event) => {
-      console.log(event.target);
-    };
+  let navLinks = document.querySelectorAll("nav li");
+
+  navLinks.forEach((element) => {
+    clickStyle(element);
   });
+};
+
+const clickStyle = (element) => {
+  console.log(element);
+  element.onclick = (event) => {
+    let newElem = event.target;
+    newElem.classList.add("nav-li-clicked");
+
+    setTimeout(() => {
+      newElem.classList.remove("nav-li-clicked");
+    }, 200);
+  };
 };
 
 export { navbarLoad };
