18cc1864125bc4b9e94ca15e4f2baebd6ca1a3aa
Mihail Marian <mihailmarian12@gmail.com>
2020-11-18 14:35:16 +0200

    Started sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,8 @@
+import "./sidebar.scss";
+import html from "./sidebar.html";
+
 const sidebarLoad = () => {
+  document.querySelector("main").insertAdjacentHTML("afterbegin", html);
   console.log("sidebarLoad activated");
 };
 
