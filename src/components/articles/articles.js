94845e1e5b8967c0ec0527173d49b4672b6f0601
Mihail Marian <mihailmarian12@gmail.com>
2020-11-19 09:22:32 +0200

    article positioned well now

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -2,13 +2,12 @@ import "./articles.scss";
 import html from "./articles.html";
 
 const articleLoad = (articleId = "china") => {
-  let doc = new DOMParser().parseFromString(html, "text/html");
-
   let prevSection = document.querySelector("main>section");
   if (prevSection) {
     prevSection.remove();
   }
 
+  let doc = new DOMParser().parseFromString(html, "text/html");
   document.querySelector("main").appendChild(doc.getElementById(articleId));
 };
 
