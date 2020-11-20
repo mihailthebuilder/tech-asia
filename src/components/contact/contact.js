cab55ff49090de19f4c8b2fc8a207cb0ce397a34
Mihail Marian <mihailmarian12@gmail.com>
2020-11-20 09:20:29 +0200

    started contact and enabled hot module reload

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,7 +2,7 @@ import html from "./contact.html";
 import "./contact.scss";
 
 const contactLoad = () => {
-  document.querySelector("main").innerHTML = "";
+  document.querySelector("main").innerHTML = html;
 };
 
 export { contactLoad };
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
 
   entry: "./src/index.js",
