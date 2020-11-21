791b3c6fd01be02f6d2848842fc801d5b769f1fe
Mihail Marian <mihailmarian12@gmail.com>
2020-11-21 16:23:31 +0200

    fixed fade replace

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,6 +3,23 @@ import "./contact.scss";
 
 const contactLoad = () => {
   document.querySelector("main").innerHTML = html;
+
+  document
+    .getElementsByTagName("form")[0]
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+      event.target.classList.add("hide");
+
+      setTimeout(() => {
+        event.target.style.display = "none";
+        let submittedElem = document.getElementById("submitted");
+        submittedElem.style.display = "block";
+
+        setTimeout(() => {
+          submittedElem.classList.add("show");
+        }, 100);
+      }, 500);
+    });
 };
 
 export { contactLoad };
