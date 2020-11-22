77ef87059703d9d03dcf73509a43f8ae290ccca5
Mihail Marian <mihailmarian12@gmail.com>
2020-11-22 10:29:04 +0200

    stashed changes

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: "file-loader",
@@ -55,6 +55,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|jpeg)$/,
         use: "file-loader",
+      },
+
+      {
+        test: /\.svg$/i,
+        use: "raw-loader",
       },
 
       {
